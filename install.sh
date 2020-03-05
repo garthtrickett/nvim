77e6b9788a446d31b614982d71118bf52a5210b0
Garth Trickett <garthtrickett@gmail.com>
2020-03-05 07:54:08 +0000

    lots more config and plugins

--- install.sh
+++ install.sh
@@ -2,3 +2,17 @@ wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
 chmod +x /usr/bin/nvim
 nvim --headless +PlugInstall +qa
+source activate ProdigyAI
+python3 -m pip install pynvim
+python3 -m pip install jedi
+python3 -m pip install msgpack
+python3 -m pip install --upgrade msgpack
+python3 -m pip install yapf
+python3 -m pip install flake 8
+sudo apt install tmux
+curl -sL install-node.now.sh/lts | bash
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install nodejs
+nvim --headless :CocInstall coc-python +qa
+
+
